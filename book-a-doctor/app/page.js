d6114ea69de96dd6e0793436fda17ad8a1ef07c0
richie1988 <richiessikaonga@gmail.com>
2024-05-03 21:16:19 +0200

    Header

--- book-a-doctor/app/page.js
+++ book-a-doctor/app/page.js
@@ -3,8 +3,6 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h2>Wellcome Richard</h2>
-      <Button>Lets Next</Button>
     </div>
   );
 }
