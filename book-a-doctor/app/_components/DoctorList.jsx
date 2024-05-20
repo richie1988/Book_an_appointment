ee8929fb73572af84a0262458a0a2140a67eae2b
richie1988 <richiessikaonga@gmail.com>
2024-05-21 00:20:28 +0200

    Doctors populating

--- book-a-doctor/app/_components/DoctorList.jsx
+++ book-a-doctor/app/_components/DoctorList.jsx
@@ -6,9 +6,10 @@ function DoctorList({doctorList}){
         <div className='md-10 px-8'>
             <h2 className='font-bold text-xl'>Popular Doctors</h2>
             <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-            gap-7'>
+            gap-7 mt-4'>
             {doctorList && doctorList.map((doctor, index) => (
-    <div key={index} className='border-[1px] rounded-lg p-4'>
+    <div key={index} className='border-[1px] rounded-lg p-3 cursor-pointer hover:border-primary 
+    hover:shadow-sm transition-all ease-in-out'>
         {doctor.attributes?.Image?.data?.[0]?.attributes?.url ? (
             <Image 
                 src={doctor.attributes.Image.data[0].attributes.url} 
@@ -21,7 +22,7 @@ function DoctorList({doctorList}){
         ) : (
             <p>Image not available</p>
         )}
-          <div className='mt-3 items-baseline flex flex-col'>
+          <div className='mt-3 items-baseline flex flex-col gap-1'>
             <h2 className='text-[15px] bg-blue-100 p-1 rounded-full
             px-2 text-primary'>
                 {doctor.attributes?.category?.data?.attributes?.Name ? (
@@ -30,7 +31,15 @@ function DoctorList({doctorList}){
                     "Category not available"
                 )}
             </h2>
-            <h2>{doctor.attributes.Name}</h2>
+            <h2 className='font-bold'>{doctor.attributes.Name}</h2>
+            <h2 className='text-primary text-sm'>{doctor.attributes.Year_of_Experience
+}</h2>
+<h2 className='text-grey-500 text-sm'>{doctor.attributes.Address}</h2>
+<h2 className='p-2 px-3 border-[1px] border-primary text-primary rounded-full w-full 
+text-center 
+text-[11px] mt-2
+cursor-pointer
+hover:bg-primary hover:text-white'>Book Now</h2>
         </div>
     </div>
 ))}
