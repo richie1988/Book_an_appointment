d8c2adbc2cc9fff9d7a8a3daa3ed92ee04e7a14e
richie1988 <richiessikaonga@gmail.com>
2024-07-04 15:22:02 +0200

    Update

--- book-a-doctor/app/_components/Footer.jsx
+++ book-a-doctor/app/_components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Logo from '@/public/logo3.png'
 
 function Footer() {
   return (
    <footer className="bg-gray-100 mt-10">
   <div className="mx-auto max-w-5xl justify-center text-center flex flex-col items-center px-4 py-16 sm:px-6 lg:px-8">
-   <img src={Logo} alt='logo' width={200} height={50} priority/>
+   <img src='/logo3.png' alt='logo' width={200} height={50} priority/>
 
     <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum
