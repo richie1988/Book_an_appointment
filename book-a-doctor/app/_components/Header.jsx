f943af988af6464417d45c5287c415469dd5d533
richie1988 <richiessikaonga@gmail.com>
2024-07-04 15:29:31 +0200

    Update

--- book-a-doctor/app/_components/Header.jsx
+++ book-a-doctor/app/_components/Header.jsx
@@ -60,7 +60,7 @@ console.log(user)
     <header className='sticky top-0 bg-white z-50 shadow-md'>
     <div className='flex justify-between p-4 shadow-sm'>
     <div className='flex items-center gap-10'>
-   <Image src="/logo3.png" alt="logo" width={120} height={50} priority/>
+   <Image src="/Logo.jpg" alt="logo" width={120} height={50} priority/>
    <ul className='md:flex gap-8 hidden'>
     {Menu.map((item,index)=> (
         <Link href={item.path}>
