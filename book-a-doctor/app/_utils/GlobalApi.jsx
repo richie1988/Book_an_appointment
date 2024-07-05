6d7fe1d1a42ff43a95c5e0132c109ada87dde79e
richie1988 <richiessikaonga@gmail.com>
2024-07-05 19:47:05 +0200

    Update api

--- book-a-doctor/app/_utils/GlobalApi.jsx
+++ book-a-doctor/app/_utils/GlobalApi.jsx
@@ -4,7 +4,7 @@ const { default: axios } = require("axios");
 
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 const axiosClient = axios.create({
-    baseURL: "https://book-a-doctor-appointment-admin.onrender.com/api",
+    baseURL: "https://book-a-doctor-admin.onrender.com/api",
     headers: {
         Authorization: `Bearer ${API_KEY}`
     }
